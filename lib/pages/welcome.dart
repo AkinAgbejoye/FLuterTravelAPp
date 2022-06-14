a0faba0c2a12c9d935f0f6125c434926b86c415b
unknown <akinwun@gmail.com>
2022-06-14 10:38:57 +0100

    onboarding page

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  List images = ["a.jpg", "b.jpg", "c.png"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      // ignore: prefer_interpolation_to_compose_strings
+                      image: AssetImage("img/" + images[index]))),
+            );
+          }),
+    );
+  }
+}
