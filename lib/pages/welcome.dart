27d9ed153342ce8ab0466d07197625969ca6fb5a
unknown <akinwun@gmail.com>
2022-07-23 13:27:03 +0100

    Slider dots added

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -53,6 +53,20 @@ class _WelcomeState extends State<Welcome> {
                         ),
                         Btn()
                       ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexDots
+                                  ? AppColors.mainColor
+                                  : AppColors.mainColor.withOpacity(0.3)),
+                        );
+                      }),
                     )
                   ],
                 ),
